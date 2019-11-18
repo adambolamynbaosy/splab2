86d3a62799a1df9730acedc5682e234dcb479bb6
adambolamynbaosy <57068140+adambolamynbaosy@users.noreply.github.com>
2019-11-18 19:22:51 +0600

    Add files via upload

---
+++ add.js
@@ -0,0 +1,4 @@
+var a = process.argv[2];
+var b = process.argv[3];
+var result = a*b;
+console.log("nodejs: " + process.argv[2] +  "*" + process.argv[3] + "=" + result);
---
+++ add.sh
@@ -0,0 +1,2 @@
+sum=`echo "$1 / $2" | bc`
+echo "bash: $1/$2=$sum"
